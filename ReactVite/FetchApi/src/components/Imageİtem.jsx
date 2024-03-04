71724657703bc94cb0447284c96348e3749257c7
0MeMo07 <omemo0135@gmail.com>
2024-03-04 15:18:23 +0300

    photo search

--- ReactVite/FetchApi/src/components/Imageİtem.jsx
+++ ReactVite/FetchApi/src/components/Imageİtem.jsx
@@ -3,8 +3,11 @@ function İmageItem({imageİtems}) {
     console.log(imageİtems)
     return ( 
         <>
+        
         <img className="imageListImg" src={imageİtems.urls.small} alt={imageİtems.alt_description} />
+
         </>
+    
      );
 }
 
